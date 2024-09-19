fd3eb6ffffb4e2ef4dc1bc93a0a9d6aebeddc1d4
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-09-20 00:20:19 +0530

    Update cin_cout.cpp

--- C++/operator_overloading/cin_cout.cpp
+++ C++/operator_overloading/cin_cout.cpp
@@ -1,1 +1,20 @@
-
+class cout_cin{
+private:
+  int i;
+public:
+  friend ostream& operator<<(ostream& obj, cout_cin &obj);
+  friend ostream& operator<<(ostream& obj, cout_cin &obj);
+};
+ostream& operator<<(ostream& obj, cout_cin &obj){
+  out<<obj.i;
+  return out;
+}
+istream& operator<<(istream& obj, cout_cin &obj){
+  out>>obj.i;
+  return out;
+}
+int main()
+{
+  cout_cin obj;
+  cout<<obj;
+}
