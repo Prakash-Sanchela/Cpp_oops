4dd9232a6023a86dcb8017d6d3d7f18811fc7087
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-10-08 11:34:38 +0530

    Create uefi_questions.cpp

---
+++ resume/uefi_questions.cpp
@@ -0,0 +1,6 @@
+Explain about the award you've got for UEFI support for Imaging suite
+Limitation of Legacy BIOS
+What is your project spcific limitation which is overcome by UEFI support
+How you've implemented UEFI support
+Advantages of using UEFI
+What is secure boot?
