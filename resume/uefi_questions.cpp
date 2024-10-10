1a4e7f39781845ce845b00aa9cb4a01b770f9fb9
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-10-10 16:02:04 +0530

    Update uefi_questions.cpp

--- resume/uefi_questions.cpp
+++ resume/uefi_questions.cpp
@@ -2,5 +2,6 @@ Explain about the award you've got for UEFI support for Imaging suite
 Limitation of Legacy BIOS
 What is your project spcific limitation which is overcome by UEFI support
 How you've implemented UEFI support
+What is CSM (Compatibility Support Module) -> to support Legacy BIOS as well (backward compatibility)
 Advantages of using UEFI
 What is secure boot?
