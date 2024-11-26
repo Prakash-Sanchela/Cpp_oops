19827e40d6d0ad34f6a4f68dc65161cb0392947e
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-11-26 22:43:58 +0530

    Create map_vs_hashmap.cpp

---
+++ stl/map_vs_hashmap.cpp
@@ -0,0 +1,8 @@
+// How map and hash map implemented internally
+// How order is being maintained in map?
+// How data is being stored actually in both map and hash map?
+// How data conflicting handled in hash map?
+
+1. Map -> red black tree
+   hashmap -> hash tables
+2. Order is being maintained using comparator operator and using red black tree where left side of element is less then the root.
