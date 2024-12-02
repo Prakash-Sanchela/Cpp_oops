15c6f65e10003541a5e90c60661eda147e89d1b5
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-12-02 20:52:40 +0530

    Update swap_bits_in_integer.cpp

--- swap_bits_in_integer.cpp
+++ swap_bits_in_integer.cpp
@@ -1,1 +1,22 @@
-
+#include<stdio.h> 
+// 10 - 0000 1010
+// pos1 = 1
+// pos2 = 4
+// After swap 
+// 24 - 0001 1000
+void swap_bits(int num, int pos1, int pos2)
+{
+    int mask1,mask2;
+    int bit= (num>>pos1)&1;
+    int bit1= (num>>pos2)&1;
+    if(bit!=bit1)
+    {
+      num^=(1<<pos1);
+      num^=(1<<pos2);
+    }
+    printf("%d ",num);
+}
+int main()
+{
+	swap_bits(10, 1, 4);
+}
