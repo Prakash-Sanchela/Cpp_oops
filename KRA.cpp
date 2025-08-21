52d64ff69ebfe467b643209721bca8b44e75f63c
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:30:39 +0530

    Create KRA.cpp

---
+++ KRA.cpp
@@ -0,0 +1,38 @@
+KRA-1 Reviewer 1
+Prakash has exhibited outstanding performance and dedication across multiple facets of his role. Here is a summary of his key contributions: 
+
+Arista Project Involvement: Prakash has been working on the Arista Project, which is billable, and has received positive appreciation from the client for his efforts and results. 
+
+Operational Excellence: He has maintained an impressive 95% Out-of-Time Report (OOTR), reflecting his reliability and efficiency in project execution. 
+
+Technical Expertise: Prakash quickly got onboard with Arista products and has been actively working on various Arista topologies. He developed automation scripts to test different Arista topologies nightly, showcasing his technical proficiency and innovation.
+
+ Mentorship and Team Support: He has played a crucial role in mentoring and supporting the team throughout the Arista Project, assisting team members during onboarding and throughout the project's lifecycle.
+
+Deliverable Tracking and Documentation: Prakash has effectively tracked deliverables, achieving at least 90% delivery, and has adhered to client documentation standards with 100% compliance. Communication and Participation: He has demonstrated 100% participation and punctuality in both team and client communications, ensuring clear and effective interactions with all stakeholders. 
+
+Client Communication: Prakash has maintained clear communication with the client on behalf of the team, facilitating smooth collaboration and project progress.
+
+
+KRA 2
+Prakash consistently demonstrates excellent team communication skills. 
+
+He comes prepared with detailed updates and insights, actively participates in discussions, and provides constructive updates during customer calls. 
+
+Prakash is a highly dependable team member who consistently meets his commitments. 
+
+He manages his tasks efficiently and delivers work on time.
+
+ Additionally, he has been instrumental in assisting other team members when they encounter challenges.
+
+KRA-3
+L1 Prakash's strong mentorship skills and dedication to team integration have been highly commendable, particularly evident in his role as a buddy to new employees. 
+
+I look forward to seeing him expand his contributions further by engaging in process improvement initiatives and taking on internal projects.
+
+L2 
+Need participation in org level initiativies like blogs,podcast,whitepapers as per the KRA set. Thanks for conducting a buddy-mentor relationship hence rating as sometimes meet expectations.
+
+KRA-4
+Prakash has detailed understanding of tasks on which he is working. He has demonstrated excellent problem-solving skills. He has completed training for CCNA and pursuing his certification. 
+
